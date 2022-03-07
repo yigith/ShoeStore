2e0e6917bff30e8e6721d41bd506aab23906188c
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2022-03-07 13:05:32 +0300

    basket model

---
+++ src/ApplicationCore/Entities/BasketItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ApplicationCore.Entities
+{
+    public class BasketItem : BaseEntity
+    {
+        public int Quantity { get; set; }
+
+        public int BasketId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
---
+++ src/Infrastructure/Data/Config/BasketItemConfig.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    public class BasketItemConfig : IEntityTypeConfiguration<BasketItem>
+    {
+        public void Configure(EntityTypeBuilder<BasketItem> builder)
+        {
+        }
+    }
+}
