9feb4c7e208db6f23face5f4db296edd8dba63ed
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2022-03-14 11:10:51 +0300

    checkout basket view model

--- src/Web/ViewComponents/CheckoutBasketViewComponent.cs
+++ src/Web/ViewComponents/CheckoutBasketViewComponent.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Web.Interfaces;
 
 namespace Web.ViewComponents
 {
     public class CheckoutBasketViewComponent : ViewComponent
     {
+        private readonly IBasketViewModelService _basketViewModelService;
+
+        public CheckoutBasketViewComponent(IBasketViewModelService basketViewModelService)
+        {
+            _basketViewModelService = basketViewModelService;
+        }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            return View(await _basketViewModelService.GetBasketViewModelAsync());
         }
     }
 }
